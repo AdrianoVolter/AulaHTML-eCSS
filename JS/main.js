40d583a9c45238600a7fb2c8e95fb5f0e3642235
AdrianoVolter <adriano.j.volter@hotmail.com>
2021-10-05 13:45:04 -0300

    aula java script

---
+++ JS/main.js
@@ -0,0 +1,29 @@
+var frutas = [{nome:"maça", cor:"vermelha"}, {nome:"uva", cor:"roxa"}];
+alert(frutas[1].nome);
+
+
+
+
+//var fruta = {nome:"maça", cor:"vermelha"}
+//console.log(fruta);
+//alert(fruta.cor);
+
+
+
+//var lista = ["maça", "pera", "laranja"];
+//lista.push("uva")
+//lista.pop();
+
+//console.log(lista.reverse());
+//console.log(lista.toLocaleString())
+//console.log(lista.join("|"))
+
+//var nome = "Adriano Volter";
+//var idade = 37;
+//var idade2 = 10;
+//var frase = "japao e o melhot time do mundo"
+//alert (nome + " tem " + idade +" anos ");
+//alert ( idade + idade2 );
+//console.log(nome);
+//console.log(idade + idade2);
+//console.log(frase.replace("japao", "brasil"));
