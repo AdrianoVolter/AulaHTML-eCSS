41728fa01a6198d49a3e104911fcd0990b875c25
AdrianoVolter <adriano.j.volter@hotmail.com>
2021-10-05 21:53:54 -0300

    aula JS

--- JS/main.js
+++ JS/main.js
@@ -1,14 +1,36 @@
-var frutas = [{nome:"maça", cor:"vermelha"}, {nome:"uva", cor:"roxa"}];
-alert(frutas[1].nome);
 
 
+//var d = new Date();
+//alert(d.getFullYear())
 
+//alert(d.getMonth()+1);
+
+//var count;
+//for(count=0; count <= 5; count++){
+//    alert(count);
+//};
+
+//var count = 0;
+//while(count <= 5){
+//    console.log(count);
+//    alert(count);
+//    count ++;
+//};
+
+//var idade = prompt("Qual a sua idade")
+//var idade = 18
+//if (idade >= 18){
+//    alert("maior de idade");
+//}else{
+//    alert("menor de idade");
+//};
+
+//var frutas = [{nome:"maça", cor:"vermelha"}, {nome:"uva", cor:"roxa"}];
+//alert(frutas[1].nome);
 
 //var fruta = {nome:"maça", cor:"vermelha"}
 //console.log(fruta);
 //alert(fruta.cor);
-
-
 
 //var lista = ["maça", "pera", "laranja"];
 //lista.push("uva")
